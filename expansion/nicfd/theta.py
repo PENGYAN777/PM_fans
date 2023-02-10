412048afc7858742f80404ba0f2d0d8f59478df6
PENGYAN777 <peng.yan@mail.polimi.it>
2023-02-10 17:44:41 +0100

    z4

--- expansion/nicfd/theta.py
+++ expansion/nicfd/theta.py
@@ -19,23 +19,42 @@ from matplotlib.ticker import FormatStrFormatter
 z9= pd.read_csv("z9.csv", ",", skiprows=0)
 z8= pd.read_csv("z8.csv", ",", skiprows=0)
 z7= pd.read_csv("z7.csv", ",", skiprows=0)
+z6= pd.read_csv("z6.csv", ",", skiprows=0)
+z5= pd.read_csv("z5.csv", ",", skiprows=0)
+z4= pd.read_csv("z4.csv", ",", skiprows=0)
 
 """
 1. input theta and upstream Mach number, compute downstream data
 """
 theta = np.zeros(50) # Gamma
+
 z9_m2 = np.zeros(50) 
 z8_m2 = np.zeros(50) 
 z7_m2 = np.zeros(50) 
+z6_m2 = np.zeros(50) 
+z5_m2 = np.zeros(50) 
+z4_m2 = np.zeros(50) 
+
 z9_P2 = np.zeros(50) 
 z8_P2 = np.zeros(50) 
 z7_P2 = np.zeros(50) 
+z6_P2 = np.zeros(50) 
+z5_P2 = np.zeros(50) 
+z4_P2 = np.zeros(50) 
+
 z9_T2 = np.zeros(50) 
 z8_T2 = np.zeros(50) 
 z7_T2 = np.zeros(50) 
+z6_T2 = np.zeros(50) 
+z5_T2 = np.zeros(50) 
+z4_T2 = np.zeros(50) 
+
 z9_D2 = np.zeros(50) 
 z8_D2 = np.zeros(50) 
 z7_D2 = np.zeros(50) 
+z6_D2 = np.zeros(50) 
+z5_D2 = np.zeros(50) 
+z4_D2 = np.zeros(50) 
 
 for i in range(50):
     theta[i] = i*math.pi/180 # rad
@@ -73,6 +92,40 @@ for i in range(50):
     z7_D2[i] = D2
     T2 = z7.iloc[:,4][np.argmin(abs(z7.iloc[:,6]-nu2))] 
     z7_T2[i] = T2
+    # for z6
+    nu1 = z6.iloc[:,6][np.argmin(abs(z6.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z6.iloc[:,5][np.argmin(abs(z6.iloc[:,6]-nu2))] 
+    z6_m2[i] = M2
+    P2 = z6.iloc[:,2][np.argmin(abs(z6.iloc[:,6]-nu2))] 
+    z6_P2[i] = P2
+    D2 = z6.iloc[:,3][np.argmin(abs(z6.iloc[:,6]-nu2))] 
+    z6_D2[i] = D2
+    T2 = z6.iloc[:,4][np.argmin(abs(z6.iloc[:,6]-nu2))] 
+    z6_T2[i] = T2
+    # for z5
+    nu1 = z5.iloc[:,6][np.argmin(abs(z5.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z5.iloc[:,5][np.argmin(abs(z5.iloc[:,6]-nu2))] 
+    z5_m2[i] = M2
+    P2 = z5.iloc[:,2][np.argmin(abs(z5.iloc[:,6]-nu2))] 
+    z5_P2[i] = P2
+    D2 = z5.iloc[:,3][np.argmin(abs(z5.iloc[:,6]-nu2))] 
+    z5_D2[i] = D2
+    T2 = z5.iloc[:,4][np.argmin(abs(z5.iloc[:,6]-nu2))] 
+    z5_T2[i] = T2
+    # for z4
+    nu1 = z4.iloc[:,6][np.argmin(abs(z4.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z4.iloc[:,5][np.argmin(abs(z4.iloc[:,6]-nu2))] 
+    z4_m2[i] = M2
+    P2 = z4.iloc[:,2][np.argmin(abs(z4.iloc[:,6]-nu2))] 
+    z4_P2[i] = P2
+    D2 = z4.iloc[:,3][np.argmin(abs(z4.iloc[:,6]-nu2))] 
+    z4_D2[i] = D2
+    T2 = z4.iloc[:,4][np.argmin(abs(z4.iloc[:,6]-nu2))] 
+    z4_T2[i] = T2
+    
     
 
 
@@ -85,7 +138,9 @@ axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(theta , z9_m2 , 'k', lw=lwh, label="$Z_t = 0.9$")
 axes.plot(theta , z8_m2 , 'r', lw=lwh, label="$Z_t = 0.8$")
 axes.plot(theta , z7_m2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-
+axes.plot(theta , z6_m2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
+axes.plot(theta , z5_m2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
+axes.plot(theta , z4_m2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
 
 axes.set_xlabel('$\\theta$(rad)',fontsize=12)
 axes.set_ylabel('$M_2$',fontsize=12) 
@@ -99,6 +154,9 @@ axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(theta , z9_P2 , 'k', lw=lwh, label="$Z_t = 0.9$")
 axes.plot(theta , z8_P2 , 'r', lw=lwh, label="$Z_t = 0.8$")
 axes.plot(theta , z7_P2 , 'b', lw=lwh, label="$Z_t = 0.7$")
+axes.plot(theta , z6_P2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
+axes.plot(theta , z5_P2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
+axes.plot(theta , z4_P2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
 
 
 axes.set_xlabel('$\\theta$(rad)',fontsize=12)
@@ -113,7 +171,9 @@ axes = fig3.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(theta , z9_T2 , 'k', lw=lwh, label="$Z_t = 0.9$")
 axes.plot(theta , z8_T2 , 'r', lw=lwh, label="$Z_t = 0.8$")
 axes.plot(theta , z7_T2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-
+axes.plot(theta , z6_T2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
+axes.plot(theta , z5_T2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
+axes.plot(theta , z4_T2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
 
 axes.set_xlabel('$\\theta$(rad)',fontsize=12)
 axes.set_ylabel('$T_2/T_c$',fontsize=12) 
@@ -127,7 +187,9 @@ axes = fig4.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(theta , z9_D2 , 'k', lw=lwh, label="$Z_t = 0.9$")
 axes.plot(theta , z8_D2 , 'r', lw=lwh, label="$Z_t = 0.8$")
 axes.plot(theta , z7_D2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-
+axes.plot(theta , z6_D2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
+axes.plot(theta , z5_D2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
+axes.plot(theta , z4_D2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
 
 axes.set_xlabel('$\\theta$(rad)',fontsize=12)
 axes.set_ylabel('$\\rho_2/\\rho_c$',fontsize=12) 
---
+++ expansion/similar/TS.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 26 16:05:08 2022
+
+@author: yan
+
+plot (P,T) diagram. show contour of compressibility factor Z and 
+fundamental derivative of gasdynamics Gamma
+"""
+
+import matplotlib
+import numpy as np
+import CoolProp as CP
+import matplotlib.pyplot as plt
+import scipy.interpolate
+
+
+# give fluid name
+fluidname = "MM"
+
+# update fluid
+fluid = CP.AbstractState("HEOS", fluidname)
+
+pc = fluid.keyed_output(CP.iP_critical)
+Tc = fluid.keyed_output(CP.iT_critical)
+ec = CP.CoolProp.PropsSI('Smass','T',Tc,'P',pc,fluidname)
+Tmin =  CP.CoolProp.PropsSI("Ttriple",fluidname)
+Tmax =  CP.CoolProp.PropsSI("Tmax",fluidname)
+pmax = fluid.keyed_output(CP.iP_max)
+pmin = fluid.keyed_output(CP.iP_min)
+fillcolor = 'g'
+
+# fig = plt.figure(figsize = (6,6))
+# ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(6,5))
+left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+ax = fig.add_axes([left, bottom, width, height]) 
+lw = 3
+
+
+# ----------------
+# Saturation curve
+# ----------------
+Ts = np.linspace(Tmin, Tc, 1000)
+es = CP.CoolProp.PropsSI('Smass','T',Ts,'Q',1,fluidname)
+
+# ----------------
+# Contour of Z and Gamma
+# ----------------
+n = 200 # number of points
+y = np.linspace(Tmin, Tmax,n)
+x = np.linspace(300, 1600,n)
+X,Y = np.meshgrid(x,y)
+Z =  CP.CoolProp.PropsSI('Z','Smass',X,'T|gas',Y,fluidname)
+Gamma =  CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics','Smass',X,'T',Y,fluidname)
+#print(Z.shape)
+levels = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+cp = plt.contour(X, Y, Z, levels, colors='black', linestyles='dashed')
+plt.clabel(cp, inline=True,  fontsize=10)
+plt.contourf(X, Y, Gamma, [0.4,0.5,0.6,0.7,0.8, 0.9,1.0], cmap='rainbow')
+plt.colorbar()
+# ------
+# Labels
+# ------
+
+plt.plot(es,Ts,'k',lw = lw, solid_capstyle = 'round', label = "LVS")
+ax.legend(loc=4) # 2 means left top
+# Critical lines
+plt.axvline(ec, dashes = [2, 2])
+plt.axhline(Tc, dashes = [2, 2])
+
+"""
+test points
+"""
+z9_p = [1.55e6]
+z9_t = [673]
+z9_s = CP.CoolProp.PropsSI('Smass','P',z9_p,'T',z9_t,fluidname)
+plt.plot(z9_s,z9_t,'ko',lw = lw)
+
+z8_p = [1.55e6]
+z8_t = [584.85]
+z8_s = CP.CoolProp.PropsSI('Smass','P',z8_p,'T',z8_t,fluidname)
+plt.plot(z8_s,z8_t,'ro',lw = lw)
+
+z7_p = [2.13e6]
+z7_t = [577.21]
+z7_s = CP.CoolProp.PropsSI('Smass','P',z7_p,'T',z7_t,fluidname)
+plt.plot(z7_s,z7_t,'bo',lw = lw)
+
+plt.ylim(300,Tmax)
+# plt.gca().set_yscale('log')
+plt.gca().set_xlim(300, 1600)
+plt.xlabel('Entropy [J/K]')
+plt.ylabel('Temperature [K]')
+plt.title('Contour of Z and $\Gamma$ for siloxane MM')
+plt.tight_layout()
+fig.savefig("files/Contour_TS.pdf")
+print("plotcontour.py called")
