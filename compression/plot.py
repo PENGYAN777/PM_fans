7dcfdbdf46fb0bfe0e36bc2ac06ad3d190cff6bc
PENGYAN777 <peng.yan@mail.polimi.it>
2023-02-11 16:03:05 +0100

    11 Feb

--- compression/plot.py
+++ compression/plot.py
@@ -17,9 +17,6 @@ read csv file
 z9= pd.read_csv("z9.csv", ",", skiprows=0)
 z8= pd.read_csv("z8.csv", ",", skiprows=0)
 z7= pd.read_csv("z7.csv", ",", skiprows=0)
-z6= pd.read_csv("z6.csv", ",", skiprows=0)
-z5= pd.read_csv("z5.csv", ",", skiprows=0)
-z4= pd.read_csv("z4.csv", ",", skiprows=0)
 
 """
 plot 
@@ -30,13 +27,10 @@ axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(z9.iloc[:,3] , z9.iloc[:,5] , 'k', lw=lwh, label="$Z_t = 0.9$")
 axes.plot(z8.iloc[:,3] , z8.iloc[:,5] , 'r', lw=lwh, label="$Z_t = 0.8$")
 axes.plot(z7.iloc[:,3] , z7.iloc[:,5] , 'b', lw=lwh, label="$Z_t = 0.7$")
-axes.plot(z6.iloc[:,3] , z6.iloc[:,5] , 'k--', lw=lwh, label="$Z_t = 0.6$")
-axes.plot(z5.iloc[:,3] , z5.iloc[:,5] , 'r--', lw=lwh, label="$Z_t = 0.5$")
-axes.plot(z4.iloc[:,3] , z4.iloc[:,5] , 'b--', lw=lwh, label="$Z_t = 0.4$")
 
-axes.set_xlabel('$\\rho/\\rho_t$',fontsize=12)
+axes.set_xlabel('$\\rho/\\rho_c$',fontsize=12)
 axes.set_ylabel('Mach',fontsize=12) 
-axes.set_title('Mach number vs $\\rho/\\rho_t$',fontsize=14)
+axes.set_title('Mach number vs $\\rho/\\rho_c$',fontsize=14)
 axes.legend(loc=0 , prop={'size': 10}) # 
 fig1.savefig("Mach_rho.pdf")
 
@@ -46,9 +40,6 @@ axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
 axes.plot(z9.iloc[:,5] , z9.iloc[:,6] , 'k', lw=lwh, label="$Z_t = 0.9$")
 axes.plot(z8.iloc[:,5] , z8.iloc[:,6] , 'r', lw=lwh, label="$Z_t = 0.8$")
 axes.plot(z7.iloc[:,5] , z7.iloc[:,6] , 'b', lw=lwh, label="$Z_t = 0.7$")
-axes.plot(z6.iloc[:,5] , z6.iloc[:,6] , 'k--', lw=lwh, label="$Z_t = 0.6$")
-axes.plot(z5.iloc[:,5] , z5.iloc[:,6] , 'r--', lw=lwh, label="$Z_t = 0.5$")
-axes.plot(z4.iloc[:,5] , z4.iloc[:,6] , 'b--', lw=lwh, label="$Z_t = 0.4$")
 
 axes.set_xlabel('Mach',fontsize=12)
 axes.set_ylabel('$\\nu$',fontsize=12) 
--- compression/theta.py
+++ compression/theta.py
@@ -26,106 +26,77 @@ z4= pd.read_csv("z4.csv", ",", skiprows=0)
 """
 1. input theta and upstream Mach number, compute downstream data
 """
-theta = np.zeros(50) # Gamma
+n = 20
+theta = np.zeros(n) # Gamma
 
-z9_m2 = np.zeros(50) 
-z8_m2 = np.zeros(50) 
-z7_m2 = np.zeros(50) 
-z6_m2 = np.zeros(50) 
-z5_m2 = np.zeros(50) 
-z4_m2 = np.zeros(50) 
+z9_m2 = np.zeros(n) 
+z8_m2 = np.zeros(n) 
+z7_m2 = np.zeros(n) 
+z6_m2 = np.zeros(n) 
+z5_m2 = np.zeros(n) 
+z4_m2 = np.zeros(n) 
 
-z9_P2 = np.zeros(50) 
-z8_P2 = np.zeros(50) 
-z7_P2 = np.zeros(50) 
-z6_P2 = np.zeros(50) 
-z5_P2 = np.zeros(50) 
-z4_P2 = np.zeros(50) 
+z9_P2 = np.zeros(n) 
+z8_P2 = np.zeros(n) 
+z7_P2 = np.zeros(n) 
+z6_P2 = np.zeros(n) 
+z5_P2 = np.zeros(n) 
+z4_P2 = np.zeros(n) 
 
-z9_T2 = np.zeros(50) 
-z8_T2 = np.zeros(50) 
-z7_T2 = np.zeros(50) 
-z6_T2 = np.zeros(50) 
-z5_T2 = np.zeros(50) 
-z4_T2 = np.zeros(50) 
 
-z9_D2 = np.zeros(50) 
-z8_D2 = np.zeros(50) 
-z7_D2 = np.zeros(50) 
-z6_D2 = np.zeros(50) 
-z5_D2 = np.zeros(50) 
-z4_D2 = np.zeros(50) 
-
-for i in range(50):
+for i in range(n):
     theta[i] = i*math.pi/180 # rad
-    M1 = 1.1 # upstream Mach number
+    M1 = 1.6 # upstream Mach number
     # for z9
     nu1 = z9.iloc[:,6][np.argmin(abs(z9.iloc[:,5]-M1))] 
-    nu2 = nu1 + theta[i]
+    nu2 = nu1 - theta[i]
     M2 = z9.iloc[:,5][np.argmin(abs(z9.iloc[:,6]-nu2))] 
     z9_m2[i] = M2
     P2 = z9.iloc[:,2][np.argmin(abs(z9.iloc[:,6]-nu2))] 
     z9_P2[i] = P2
-    D2 = z9.iloc[:,3][np.argmin(abs(z9.iloc[:,6]-nu2))] 
-    z9_D2[i] = D2
-    T2 = z9.iloc[:,4][np.argmin(abs(z9.iloc[:,6]-nu2))] 
-    z9_T2[i] = T2
+    # D2 = z9.iloc[:,3][np.argmin(abs(z9.iloc[:,6]-nu2))] 
+    # z9_D2[i] = D2
+    # T2 = z9.iloc[:,4][np.argmin(abs(z9.iloc[:,6]-nu2))] 
+    # z9_T2[i] = T2
     # for z8
     nu1 = z8.iloc[:,6][np.argmin(abs(z8.iloc[:,5]-M1))] 
-    nu2 = nu1 + theta[i]
+    nu2 = nu1 - theta[i]
     M2 = z8.iloc[:,5][np.argmin(abs(z8.iloc[:,6]-nu2))] 
     z8_m2[i] = M2
     P2 = z8.iloc[:,2][np.argmin(abs(z8.iloc[:,6]-nu2))] 
     z8_P2[i] = P2
-    D2 = z8.iloc[:,3][np.argmin(abs(z8.iloc[:,6]-nu2))] 
-    z8_D2[i] = D2
-    T2 = z8.iloc[:,4][np.argmin(abs(z8.iloc[:,6]-nu2))] 
-    z8_T2[i] = T2
+
     # for z7
     nu1 = z7.iloc[:,6][np.argmin(abs(z7.iloc[:,5]-M1))] 
-    nu2 = nu1 + theta[i]
+    nu2 = nu1 - theta[i]
     M2 = z7.iloc[:,5][np.argmin(abs(z7.iloc[:,6]-nu2))] 
     z7_m2[i] = M2
     P2 = z7.iloc[:,2][np.argmin(abs(z7.iloc[:,6]-nu2))] 
     z7_P2[i] = P2
-    D2 = z7.iloc[:,3][np.argmin(abs(z7.iloc[:,6]-nu2))] 
-    z7_D2[i] = D2
-    T2 = z7.iloc[:,4][np.argmin(abs(z7.iloc[:,6]-nu2))] 
-    z7_T2[i] = T2
+
     # for z6
     nu1 = z6.iloc[:,6][np.argmin(abs(z6.iloc[:,5]-M1))] 
-    nu2 = nu1 + theta[i]
+    nu2 = nu1 - theta[i]
     M2 = z6.iloc[:,5][np.argmin(abs(z6.iloc[:,6]-nu2))] 
     z6_m2[i] = M2
     P2 = z6.iloc[:,2][np.argmin(abs(z6.iloc[:,6]-nu2))] 
     z6_P2[i] = P2
-    D2 = z6.iloc[:,3][np.argmin(abs(z6.iloc[:,6]-nu2))] 
-    z6_D2[i] = D2
-    T2 = z6.iloc[:,4][np.argmin(abs(z6.iloc[:,6]-nu2))] 
-    z6_T2[i] = T2
+    
     # for z5
     nu1 = z5.iloc[:,6][np.argmin(abs(z5.iloc[:,5]-M1))] 
-    nu2 = nu1 + theta[i]
+    nu2 = nu1 - theta[i]
     M2 = z5.iloc[:,5][np.argmin(abs(z5.iloc[:,6]-nu2))] 
     z5_m2[i] = M2
     P2 = z5.iloc[:,2][np.argmin(abs(z5.iloc[:,6]-nu2))] 
     z5_P2[i] = P2
-    D2 = z5.iloc[:,3][np.argmin(abs(z5.iloc[:,6]-nu2))] 
-    z5_D2[i] = D2
-    T2 = z5.iloc[:,4][np.argmin(abs(z5.iloc[:,6]-nu2))] 
-    z5_T2[i] = T2
+    
     # for z4
     nu1 = z4.iloc[:,6][np.argmin(abs(z4.iloc[:,5]-M1))] 
-    nu2 = nu1 + theta[i]
+    nu2 = nu1 - theta[i]
     M2 = z4.iloc[:,5][np.argmin(abs(z4.iloc[:,6]-nu2))] 
     z4_m2[i] = M2
     P2 = z4.iloc[:,2][np.argmin(abs(z4.iloc[:,6]-nu2))] 
     z4_P2[i] = P2
-    D2 = z4.iloc[:,3][np.argmin(abs(z4.iloc[:,6]-nu2))] 
-    z4_D2[i] = D2
-    T2 = z4.iloc[:,4][np.argmin(abs(z4.iloc[:,6]-nu2))] 
-    z4_T2[i] = T2
-    
     
 
 
@@ -135,12 +106,13 @@ for i in range(50):
 fig1 = plt.figure( dpi=300)
 lwh = 2
 axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
-axes.plot(theta/math.pi*180  , z9_m2 , 'k', lw=lwh, label="$Z_t = 0.9$")
-axes.plot(theta/math.pi*180  , z8_m2 , 'r', lw=lwh, label="$Z_t = 0.8$")
-axes.plot(theta/math.pi*180  , z7_m2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-axes.plot(theta/math.pi*180  , z6_m2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
-axes.plot(theta/math.pi*180  , z5_m2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
-axes.plot(theta/math.pi*180  , z4_m2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
+axes.plot(theta/math.pi*180 , z9_m2 , 'k', lw=lwh, label="$Z_t = 0.9$")
+axes.plot(theta/math.pi*180 , z8_m2 , 'r', lw=lwh, label="$Z_t = 0.8$")
+axes.plot(theta/math.pi*180 , z7_m2 , 'b', lw=lwh, label="$Z_t = 0.7$")
+axes.plot(theta/math.pi*180 , z6_m2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
+axes.plot(theta/math.pi*180 , z5_m2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
+axes.plot(theta/math.pi*180 , z4_m2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
+
 
 axes.set_xlabel('$\\theta$(degree)',fontsize=12)
 axes.set_ylabel('$M_2$',fontsize=12) 
@@ -151,15 +123,15 @@ fig1.savefig("M2_theta.pdf")
 fig2 = plt.figure( dpi=300)
 lwh = 2
 axes = fig2.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
-axes.plot(theta/math.pi*180  , z9_P2 , 'k', lw=lwh, label="$Z_t = 0.9$")
-axes.plot(theta/math.pi*180  , z8_P2 , 'r', lw=lwh, label="$Z_t = 0.8$")
-axes.plot(theta/math.pi*180  , z7_P2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-axes.plot(theta/math.pi*180  , z6_P2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
-axes.plot(theta/math.pi*180  , z5_P2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
-axes.plot(theta/math.pi*180  , z4_P2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
+axes.plot(theta/math.pi*180 , z9_P2 , 'k', lw=lwh, label="$Z_t = 0.9$")
+axes.plot(theta/math.pi*180 , z8_P2 , 'r', lw=lwh, label="$Z_t = 0.8$")
+axes.plot(theta/math.pi*180 , z7_P2 , 'b', lw=lwh, label="$Z_t = 0.7$")
+axes.plot(theta/math.pi*180 , z6_P2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
+axes.plot(theta/math.pi*180 , z5_P2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
+axes.plot(theta/math.pi*180 , z4_P2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
+# 
 
-
-axes.set_xlabel('$\\theta$(rdegree)',fontsize=12)
+axes.set_xlabel('$\\theta$(degree)',fontsize=12)
 axes.set_ylabel('$P_2/P_t$',fontsize=12) 
 axes.set_title('$P_2/P_t$ vs $\\theta$',fontsize=14)
 axes.legend(loc=0 , prop={'size': 10}) # 
@@ -171,9 +143,7 @@ fig2.savefig("P2_theta.pdf")
 # axes.plot(theta , z9_T2 , 'k', lw=lwh, label="$Z_t = 0.9$")
 # axes.plot(theta , z8_T2 , 'r', lw=lwh, label="$Z_t = 0.8$")
 # axes.plot(theta , z7_T2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-# axes.plot(theta , z6_T2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
-# # axes.plot(theta , z5_T2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
-# # axes.plot(theta , z4_T2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
+
 
 # axes.set_xlabel('$\\theta$(rad)',fontsize=12)
 # axes.set_ylabel('$T_2/T_c$',fontsize=12) 
@@ -187,9 +157,7 @@ fig2.savefig("P2_theta.pdf")
 # axes.plot(theta , z9_D2 , 'k', lw=lwh, label="$Z_t = 0.9$")
 # axes.plot(theta , z8_D2 , 'r', lw=lwh, label="$Z_t = 0.8$")
 # axes.plot(theta , z7_D2 , 'b', lw=lwh, label="$Z_t = 0.7$")
-# axes.plot(theta , z6_D2 , 'k--', lw=lwh, label="$Z_t = 0.6$")
-# # axes.plot(theta , z5_D2 , 'r--', lw=lwh, label="$Z_t = 0.5$")
-# # axes.plot(theta , z4_D2 , 'b--', lw=lwh, label="$Z_t = 0.4$")
+
 
 # axes.set_xlabel('$\\theta$(rad)',fontsize=12)
 # axes.set_ylabel('$\\rho_2/\\rho_c$',fontsize=12) 
