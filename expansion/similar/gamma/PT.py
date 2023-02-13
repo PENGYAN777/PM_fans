e4292f06763f620d2db189f3d3983cf123d0de76
PENGYAN777 <peng.yan@mail.polimi.it>
2023-02-13 11:29:17 +0100

    g

---
+++ expansion/similar/gamma/PT.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 26 16:05:08 2022
+
+@author: yan
+
+plot (P,T) diagram. show contour of compressibility factor Z and 
+fundamental derivative of gasdynamics Gamma
+"""
+
+import matplotlib
+import numpy as np
+import CoolProp as CP
+import matplotlib.pyplot as plt
+import scipy.interpolate
+
+
+# give fluid name
+fluidname = "MM"
+
+# update fluid
+fluid = CP.AbstractState("HEOS", fluidname)
+
+pc = fluid.keyed_output(CP.iP_critical)
+Tc = fluid.keyed_output(CP.iT_critical)
+Tmin =  CP.CoolProp.PropsSI("Ttriple",fluidname)
+Tmax =  CP.CoolProp.PropsSI("Tmax",fluidname)
+pmax = fluid.keyed_output(CP.iP_max)
+pmin = fluid.keyed_output(CP.iP_min)
+fillcolor = 'g'
+
+# fig = plt.figure(figsize = (6,6))
+# ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(6,5))
+left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+ax = fig.add_axes([left, bottom, width, height]) 
+lw = 3
+
+
+# ----------------
+# Saturation curve
+# ----------------
+Ts = np.linspace(Tmin, Tc, 1000)
+ps = CP.CoolProp.PropsSI('P','T',Ts,'Q',0,fluidname)
+
+# ----------------
+# Contour of Z and Gamma
+# ----------------
+n = 200 # number of points
+x = np.linspace(Tmin, Tmax,n)
+y = np.linspace(1e5, 1e7,n)
+X,Y = np.meshgrid(x,y)
+Z =  CP.CoolProp.PropsSI('Z','T',X,'P',Y,fluidname)
+Gamma =  CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics','T',X,'P',Y,fluidname)
+#print(Z.shape)
+levels = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+cp = plt.contour(X, Y, Z, levels, colors='black', linestyles='dashed')
+plt.clabel(cp, inline=True,  fontsize=10)
+plt.contourf(X, Y, Gamma, [0.4,0.5,0.6,0.7,0.8,0.9,1.0], cmap='rainbow')
+plt.colorbar()
+# ------
+# Labels
+# ------
+
+plt.plot(Ts,ps,'k',lw = lw, solid_capstyle = 'round', label = "LVS")
+ax.legend(loc=3) # 2 means left top
+# Critical lines
+plt.axvline(Tc, dashes = [2, 2])
+plt.axhline(pc, dashes = [2, 2])
+
+"""
+test points
+"""
+z9_p = [1.35e6, 2.04e6, 2.53e6, 2.84e6, 2.94e6, 2.89e6]
+z9_t = [604.06, 619.49, 616.75, 607.89, 595.31, 581.08]
+plt.plot(z9_t,z9_p,'ko',lw = lw)
+
+
+
+plt.ylim(1e5,1e7)
+plt.gca().set_yscale('log')
+plt.gca().set_xlim(Tmin, Tmax)
+plt.ylabel('Pressure [Pa]')
+plt.xlabel('Temperature [K]')
+plt.title('Contour of Z and $\Gamma$ for siloxane MM')
+plt.tight_layout()
+fig.savefig("files/Contour_PT.pdf")
+print("plotcontour.py called")
