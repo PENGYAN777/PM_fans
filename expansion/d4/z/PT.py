c459cdc4e83dcc1f628bb75dc97071b7783607a9
PENGYAN777 <peng.yan@mail.polimi.it>
2023-02-23 17:01:58 +0100

    D4

---
+++ expansion/d4/z/PT.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 26 16:05:08 2022
+
+@author: yan
+
+plot (P,T) diagram. show contour of compressibility factor Z and 
+fundamental derivative of gasdynamics Gamma
+"""
+
+import matplotlib
+import numpy as np
+import CoolProp as CP
+import matplotlib.pyplot as plt
+import scipy.interpolate
+
+
+# give fluid name
+fluidname = "D4"
+
+# update fluid
+fluid = CP.AbstractState("HEOS", fluidname)
+
+pc = fluid.keyed_output(CP.iP_critical)
+Tc = fluid.keyed_output(CP.iT_critical)
+Tmin =  CP.CoolProp.PropsSI("Ttriple",fluidname)
+Tmax =  CP.CoolProp.PropsSI("Tmax",fluidname)
+pmax = fluid.keyed_output(CP.iP_max)
+pmin = fluid.keyed_output(CP.iP_min)
+fillcolor = 'g'
+
+# fig = plt.figure(figsize = (6,6))
+# ax = fig.add_subplot(111)
+fig = plt.figure(figsize=(6,5))
+left, bottom, width, height = 0.1, 0.1, 0.8, 0.8
+ax = fig.add_axes([left, bottom, width, height]) 
+lw = 3
+
+
+# ----------------
+# Saturation curve
+# ----------------
+Ts = np.linspace(Tmin, Tc, 1000)
+ps = CP.CoolProp.PropsSI('P','T',Ts,'Q',0,fluidname)
+
+# ----------------
+# Contour of Z and Gamma
+# ----------------
+n = 200 # number of points
+x = np.linspace(400, 1000,n)
+y = np.linspace(1e5, 8e6,n)
+X,Y = np.meshgrid(x,y)
+Z =  CP.CoolProp.PropsSI('Z','T',X,'P',Y,fluidname)
+Gamma =  CP.CoolProp.PropsSI('fundamental_derivative_of_gas_dynamics','T',X,'P',Y,fluidname)
+#print(Z.shape)
+levels = [0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+cp = plt.contour(X, Y, Z, levels, colors='black', linestyles='dashed')
+plt.clabel(cp, inline=True,  fontsize=10)
+plt.contourf(X, Y, Gamma, [0.4,0.5,0.6,0.7,0.8,0.9,1.0], cmap='rainbow')
+plt.colorbar()
+# ------
+# Labels
+# ------
+
+plt.plot(Ts,ps,'k',lw = lw, solid_capstyle = 'round', label = "LVS")
+ax.legend(loc=4) # 2 means left top
+# Critical lines
+plt.axvline(Tc, dashes = [2, 2])
+plt.axhline(pc, dashes = [2, 2])
+
+"""
+test points
+"""
+nc = 10
+colors = plt.cm.tab20(np.linspace(0, 1, nc))
+
+z9_p = [1.61e6,  ] 
+z9_t = [799.20,  ]
+plt.plot(z9_t,z9_p,'o' ,color=colors[0], lw = lw)
+
+# z8_p = [7.75e5, 6.91e5,  6.55e5, 5.95e5, 5.35e5, 4.75e5]
+# z8_t = [574.29, 558.44,  551.50,  538.87, 524.85, 509.77]
+# plt.plot(z8_t,z8_p,'o' ,color=colors[1], lw = lw)
+
+# z7_p = [1.05e6, 1.00e6, 9.47e5, 8.92e5, 8.37e5, 7.82e5]
+# z7_t = [573.59, 569.74, 558.45, 552.20, 544.13, 535.72]
+# plt.plot(z7_t,z7_p,'o' ,color=colors[2], lw = lw)
+
+# z6_p = [1.269e6,1.22e6, 1.13e6, 1.11e6,  1.07e6, 1.02e6]
+# z6_t = [572.89, 569.74, 558.45, 557.11,  552.90, 547.29]
+# plt.plot(z6_t,z6_p,'o' ,color=colors[3], lw = lw)
+
+# z5_p = [1.396e6, 1.38e6, 1.24e6, 1.22e6, 1.21e6, 1.18e6, ]
+# z5_t = [570.44, 569.74, 558.16, 556.76, 555.71, 553.25, ]
+# plt.plot(z5_t,z5_p,'o' ,color=colors[4], lw = lw)
+
+
+plt.ylim(1e5,8e6)
+plt.gca().set_yscale('log')
+plt.gca().set_xlim(400, 1000)
+plt.ylabel('Pressure [Pa]')
+plt.xlabel('Temperature [K]')
+plt.title('Contour of Z and $\Gamma$ for siloxane D4')
+plt.tight_layout()
+fig.savefig("files/d4_z_Contour_PT.pdf")
+print("plotcontour.py called")
---
+++ expansion/d4/z/plot_all.py
@@ -0,0 +1,352 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 30 18:04:05 2023
+
+compute downstream Mach number  for nonideal flow
+
+@author: yan
+"""
+import numpy as np
+import pandas as pd
+import math
+import matplotlib.pyplot as plt
+
+
+"""
+0. get data
+"""
+z91= pd.read_csv("z9/z1.csv", ",", skiprows=0)
+z92= pd.read_csv("z9/z2.csv", ",", skiprows=0)
+z93= pd.read_csv("z9/z3.csv", ",", skiprows=0)
+z94= pd.read_csv("z9/z4.csv", ",", skiprows=0)
+z95= pd.read_csv("z9/z5.csv", ",", skiprows=0)
+z96= pd.read_csv("z9/z6.csv", ",", skiprows=0)
+
+z81= pd.read_csv("z8/z1.csv", ",", skiprows=0)
+z82= pd.read_csv("z8/z2.csv", ",", skiprows=0)
+z83= pd.read_csv("z8/z3.csv", ",", skiprows=0)
+z84= pd.read_csv("z8/z4.csv", ",", skiprows=0)
+z85= pd.read_csv("z8/z5.csv", ",", skiprows=0)
+z86= pd.read_csv("z8/z6.csv", ",", skiprows=0)
+
+z71= pd.read_csv("z7/z1.csv", ",", skiprows=0)
+z72= pd.read_csv("z7/z2.csv", ",", skiprows=0)
+z73= pd.read_csv("z7/z3.csv", ",", skiprows=0)
+z74= pd.read_csv("z7/z4.csv", ",", skiprows=0)
+z75= pd.read_csv("z7/z5.csv", ",", skiprows=0)
+z76= pd.read_csv("z7/z6.csv", ",", skiprows=0)
+
+z61= pd.read_csv("z6/z1.csv", ",", skiprows=0)
+z62= pd.read_csv("z6/z2.csv", ",", skiprows=0)
+z63= pd.read_csv("z6/z3.csv", ",", skiprows=0)
+z64= pd.read_csv("z6/z4.csv", ",", skiprows=0)
+z65= pd.read_csv("z6/z5.csv", ",", skiprows=0)
+z66= pd.read_csv("z6/z6.csv", ",", skiprows=0)
+
+z51= pd.read_csv("z5/z1.csv", ",", skiprows=0)
+z52= pd.read_csv("z5/z2.csv", ",", skiprows=0)
+z53= pd.read_csv("z5/z3.csv", ",", skiprows=0)
+z54= pd.read_csv("z5/z4.csv", ",", skiprows=0)
+z55= pd.read_csv("z5/z5.csv", ",", skiprows=0)
+z56= pd.read_csv("z5/z6.csv", ",", skiprows=0)
+
+
+
+"""
+1. input theta and upstream Mach number, compute downstream data
+"""
+n = 50
+"""
+Z9
+"""
+theta = np.zeros(n) # rad
+smallest = np.zeros(n) # min
+largest = np.zeros(n) # max
+diff = np.zeros(n) # diff
+
+z91_m2 = np.zeros(n) 
+z92_m2 = np.zeros(n) 
+z93_m2 = np.zeros(n) 
+z94_m2 = np.zeros(n) 
+z95_m2 = np.zeros(n) 
+z96_m2 = np.zeros(n) 
+
+for i in range(50):
+    theta[i] = i*math.pi/180 # rad
+    M1 = 1.0 # upstream Mach number
+    # for z1
+    nu1 = z91.iloc[:,6][np.argmin(abs(z91.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z91.iloc[:,5][np.argmin(abs(z91.iloc[:,6]-nu2))] 
+    z91_m2[i] = M2
+    # for z2
+    nu1 = z92.iloc[:,6][np.argmin(abs(z92.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z92.iloc[:,5][np.argmin(abs(z92.iloc[:,6]-nu2))] 
+    z92_m2[i] = M2
+    # for z3
+    nu1 = z93.iloc[:,6][np.argmin(abs(z93.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z93.iloc[:,5][np.argmin(abs(z93.iloc[:,6]-nu2))] 
+    z93_m2[i] = M2
+    # for z4
+    nu1 = z94.iloc[:,6][np.argmin(abs(z94.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z94.iloc[:,5][np.argmin(abs(z94.iloc[:,6]-nu2))] 
+    z94_m2[i] = M2
+    # for z5
+    nu1 = z95.iloc[:,6][np.argmin(abs(z95.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z95.iloc[:,5][np.argmin(abs(z95.iloc[:,6]-nu2))] 
+    z95_m2[i] = M2
+    # for z6
+    nu1 = z96.iloc[:,6][np.argmin(abs(z96.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z96.iloc[:,5][np.argmin(abs(z96.iloc[:,6]-nu2))] 
+    z96_m2[i] = M2
+"""
+Z8
+"""
+theta = np.zeros(n) # rad
+smallest = np.zeros(n) # min
+largest = np.zeros(n) # max
+diff = np.zeros(n) # diff
+
+z81_m2 = np.zeros(n) 
+z82_m2 = np.zeros(n) 
+z83_m2 = np.zeros(n) 
+z84_m2 = np.zeros(n) 
+z85_m2 = np.zeros(n) 
+z86_m2 = np.zeros(n) 
+
+for i in range(50):
+    theta[i] = i*math.pi/180 # rad
+    M1 = 1.0 # upstream Mach number
+    # for z1
+    nu1 = z81.iloc[:,6][np.argmin(abs(z81.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z81.iloc[:,5][np.argmin(abs(z81.iloc[:,6]-nu2))] 
+    z81_m2[i] = M2
+    # for z2
+    nu1 = z82.iloc[:,6][np.argmin(abs(z82.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z82.iloc[:,5][np.argmin(abs(z82.iloc[:,6]-nu2))] 
+    z82_m2[i] = M2
+    # for z3
+    nu1 = z83.iloc[:,6][np.argmin(abs(z83.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z83.iloc[:,5][np.argmin(abs(z83.iloc[:,6]-nu2))] 
+    z83_m2[i] = M2
+    # for z4
+    nu1 = z84.iloc[:,6][np.argmin(abs(z84.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z84.iloc[:,5][np.argmin(abs(z84.iloc[:,6]-nu2))] 
+    z84_m2[i] = M2
+    # for z5
+    nu1 = z85.iloc[:,6][np.argmin(abs(z85.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z85.iloc[:,5][np.argmin(abs(z85.iloc[:,6]-nu2))] 
+    z85_m2[i] = M2
+    # for z6
+    nu1 = z86.iloc[:,6][np.argmin(abs(z86.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z86.iloc[:,5][np.argmin(abs(z86.iloc[:,6]-nu2))] 
+    z86_m2[i] = M2
+"""
+Z7
+"""    
+theta = np.zeros(n) # rad
+smallest = np.zeros(n) # min
+largest = np.zeros(n) # max
+diff = np.zeros(n) # diff
+
+z71_m2 = np.zeros(n) 
+z72_m2 = np.zeros(n) 
+z73_m2 = np.zeros(n) 
+z74_m2 = np.zeros(n) 
+z75_m2 = np.zeros(n) 
+z76_m2 = np.zeros(n) 
+for i in range(50):
+    theta[i] = i*math.pi/180 # rad
+    M1 = 1.0 # upstream Mach number
+    # for z1
+    nu1 = z71.iloc[:,6][np.argmin(abs(z71.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z71.iloc[:,5][np.argmin(abs(z71.iloc[:,6]-nu2))] 
+    z71_m2[i] = M2
+    # for z2
+    nu1 = z72.iloc[:,6][np.argmin(abs(z72.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z72.iloc[:,5][np.argmin(abs(z72.iloc[:,6]-nu2))] 
+    z72_m2[i] = M2
+    # for z3
+    nu1 = z73.iloc[:,6][np.argmin(abs(z73.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z73.iloc[:,5][np.argmin(abs(z73.iloc[:,6]-nu2))] 
+    z73_m2[i] = M2
+    # for z4
+    nu1 = z74.iloc[:,6][np.argmin(abs(z74.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z74.iloc[:,5][np.argmin(abs(z74.iloc[:,6]-nu2))] 
+    z74_m2[i] = M2
+    # for z5
+    nu1 = z75.iloc[:,6][np.argmin(abs(z75.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z75.iloc[:,5][np.argmin(abs(z75.iloc[:,6]-nu2))] 
+    z75_m2[i] = M2
+    # for z6
+    nu1 = z76.iloc[:,6][np.argmin(abs(z76.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z76.iloc[:,5][np.argmin(abs(z76.iloc[:,6]-nu2))] 
+    z76_m2[i] = M2
+"""
+Z6
+"""    
+theta = np.zeros(n) # rad
+smallest = np.zeros(n) # min
+largest = np.zeros(n) # max
+diff = np.zeros(n) # diff
+
+z61_m2 = np.zeros(n) 
+z62_m2 = np.zeros(n) 
+z63_m2 = np.zeros(n) 
+z64_m2 = np.zeros(n) 
+z65_m2 = np.zeros(n) 
+z66_m2 = np.zeros(n) 
+for i in range(50):
+    theta[i] = i*math.pi/180 # rad
+    M1 = 1.0 # upstream Mach number
+    # for z1
+    nu1 = z61.iloc[:,6][np.argmin(abs(z61.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z61.iloc[:,5][np.argmin(abs(z61.iloc[:,6]-nu2))] 
+    z61_m2[i] = M2
+    # for z2
+    nu1 = z62.iloc[:,6][np.argmin(abs(z62.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z62.iloc[:,5][np.argmin(abs(z62.iloc[:,6]-nu2))] 
+    z62_m2[i] = M2
+    # for z3
+    nu1 = z63.iloc[:,6][np.argmin(abs(z63.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z63.iloc[:,5][np.argmin(abs(z63.iloc[:,6]-nu2))] 
+    z63_m2[i] = M2
+    # for z4
+    nu1 = z64.iloc[:,6][np.argmin(abs(z64.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z64.iloc[:,5][np.argmin(abs(z64.iloc[:,6]-nu2))] 
+    z64_m2[i] = M2
+    # for z5
+    nu1 = z65.iloc[:,6][np.argmin(abs(z65.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z65.iloc[:,5][np.argmin(abs(z65.iloc[:,6]-nu2))] 
+    z65_m2[i] = M2
+    # for z6
+    nu1 = z66.iloc[:,6][np.argmin(abs(z66.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z66.iloc[:,5][np.argmin(abs(z66.iloc[:,6]-nu2))] 
+    z66_m2[i] = M2
+
+"""
+Z5
+"""    
+theta = np.zeros(n) # rad
+smallest = np.zeros(n) # min
+largest = np.zeros(n) # max
+diff = np.zeros(n) # diff
+
+z51_m2 = np.zeros(n) 
+z52_m2 = np.zeros(n) 
+z53_m2 = np.zeros(n) 
+z54_m2 = np.zeros(n) 
+z55_m2 = np.zeros(n) 
+z56_m2 = np.zeros(n) 
+for i in range(50):
+    theta[i] = i*math.pi/180 # rad
+    M1 = 1.0 # upstream Mach number
+    # for z1
+    nu1 = z51.iloc[:,6][np.argmin(abs(z51.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z51.iloc[:,5][np.argmin(abs(z51.iloc[:,6]-nu2))] 
+    z51_m2[i] = M2
+    # for z2
+    nu1 = z52.iloc[:,6][np.argmin(abs(z52.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z52.iloc[:,5][np.argmin(abs(z52.iloc[:,6]-nu2))] 
+    z52_m2[i] = M2
+    # for z3
+    nu1 = z53.iloc[:,6][np.argmin(abs(z53.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z53.iloc[:,5][np.argmin(abs(z53.iloc[:,6]-nu2))] 
+    z53_m2[i] = M2
+    # for z4
+    nu1 = z54.iloc[:,6][np.argmin(abs(z54.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z54.iloc[:,5][np.argmin(abs(z54.iloc[:,6]-nu2))] 
+    z54_m2[i] = M2
+    # for z5
+    nu1 = z55.iloc[:,6][np.argmin(abs(z55.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z55.iloc[:,5][np.argmin(abs(z55.iloc[:,6]-nu2))] 
+    z55_m2[i] = M2
+    # for z6
+    nu1 = z56.iloc[:,6][np.argmin(abs(z56.iloc[:,5]-M1))] 
+    nu2 = nu1 + theta[i]
+    M2 = z56.iloc[:,5][np.argmin(abs(z56.iloc[:,6]-nu2))] 
+    z56_m2[i] = M2
+
+"""
+2. plot
+"""
+
+nc = 10
+colors = plt.cm.tab20(np.linspace(0, 1, nc))
+
+fig1 = plt.figure( dpi=300)
+lwh = 2
+axes = fig1.add_axes([0.15, 0.15, 0.7, 0.7]) #size of figure
+axes.plot(theta/math.pi*180  , z91_m2 , color=colors[0], lw=lwh, label="$Z_t=0.9$")
+axes.plot(theta/math.pi*180  , z92_m2 , color=colors[0], lw=lwh)
+axes.plot(theta/math.pi*180  , z93_m2 , color=colors[0], lw=lwh)
+axes.plot(theta/math.pi*180  , z94_m2 , color=colors[0], lw=lwh)
+axes.plot(theta/math.pi*180  , z95_m2 , color=colors[0], lw=lwh)
+axes.plot(theta/math.pi*180  , z96_m2 , color=colors[0], lw=lwh)
+
+axes.plot(theta/math.pi*180  , z81_m2 , color=colors[1], lw=lwh,label="$Z_t=0.8$")
+axes.plot(theta/math.pi*180  , z82_m2 , color=colors[1], lw=lwh)
+axes.plot(theta/math.pi*180  , z83_m2 , color=colors[1], lw=lwh)
+axes.plot(theta/math.pi*180  , z84_m2 , color=colors[1], lw=lwh)
+axes.plot(theta/math.pi*180  , z85_m2 , color=colors[1], lw=lwh)
+axes.plot(theta/math.pi*180  , z86_m2 , color=colors[1], lw=lwh)
+
+axes.plot(theta/math.pi*180  , z71_m2 , color=colors[2], lw=lwh, label="$Z_t=0.7$")
+axes.plot(theta/math.pi*180  , z72_m2 , color=colors[2], lw=lwh)
+axes.plot(theta/math.pi*180  , z73_m2 , color=colors[2], lw=lwh)
+axes.plot(theta/math.pi*180  , z74_m2 , color=colors[2], lw=lwh)
+axes.plot(theta/math.pi*180  , z75_m2 , color=colors[2], lw=lwh)
+axes.plot(theta/math.pi*180  , z76_m2 , color=colors[2], lw=lwh)
+
+axes.plot(theta/math.pi*180  , z61_m2 , color=colors[3], lw=lwh, label="$Z_t=0.6$")
+axes.plot(theta/math.pi*180  , z62_m2 , color=colors[3], lw=lwh)
+axes.plot(theta/math.pi*180  , z63_m2 , color=colors[3], lw=lwh)
+axes.plot(theta/math.pi*180  , z64_m2 , color=colors[3], lw=lwh)
+axes.plot(theta/math.pi*180  , z65_m2 , color=colors[3], lw=lwh)
+axes.plot(theta/math.pi*180  , z66_m2 , color=colors[3], lw=lwh)
+
+axes.plot(theta/math.pi*180  , z51_m2 , color=colors[4], lw=lwh, label="$Z_t=0.5$")
+axes.plot(theta/math.pi*180  , z52_m2 , color=colors[4], lw=lwh)
+axes.plot(theta/math.pi*180  , z53_m2 , color=colors[4], lw=lwh)
+axes.plot(theta/math.pi*180  , z54_m2 , color=colors[4], lw=lwh)
+axes.plot(theta/math.pi*180  , z55_m2 , color=colors[4], lw=lwh)
+axes.plot(theta/math.pi*180  , z56_m2 , color=colors[4], lw=lwh)
+
+
+axes.set_xlabel('$\\theta$(degree)',fontsize=12)
+axes.set_ylabel('$M_2$',fontsize=12) 
+# axes.set_title('$Z_t = 0.9$',fontsize=14)
+axes.legend(loc=0 , prop={'size': 10}) # 
+fig1.savefig("mdm_z_M2_theta.pdf")
+
+
+
